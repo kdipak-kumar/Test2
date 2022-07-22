9922fcf4437fda888fe464edb78b30ea70a6aa02
Dipak <kdipak005@gmail.com>
2022-07-22 06:19:01 +0530

    first commit

---
+++ src/Test.java
@@ -0,0 +1,27 @@
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		
+		findPalindrome(454);
+		
+	}
+	public static void findPalindrome(int n) {
+		 int rem=0;
+		 int sum=0;
+		 int temp=n;
+		while(n>0) {
+			rem=n%10;
+			sum=(sum*10)+rem;
+			n=n/10;
+		}
+		if(temp==sum) {
+			System.out.println(temp+" is a palindrome number");
+		}
+		else {
+			System.out.println(temp+" is not a palindrome number");
+		}
+		
+	}
+
+}
